3b66fb68cb3d54556fe07c4a4df4afd8f5712818
Brookealynn <brookeresendes@outlook.com>
2016-11-19 17:47:33 -0600

    Still trying

--- Question2.c
+++ Question2.c
@@ -5,21 +5,36 @@
 
 //Returns a new string containing the string in a followed by the string in b
 char *safestrcat(char *a, char *b) {
-	char *x; //is it keyboard input for a and b?
+	char *x; 
 	x = strcat(a,b);
 	return x; //return the new string x 
 }
 
-//int substring(char *source, int from, int n, char *target) {
-//	char target;
-//	if (strstr(source,from)) {
-		
-//	}
-//}
+//creating a parameter for the substring??
+int substring(char *source, int from, int n, char *target) {
+	int i = 0;
+	int length = 0;
+	char target[255];
+	printf("Please input a string\n");
+	gets(source); //gets string input
+
+	printf("Please enter the starting index of the substring\n");
+	gets(from); //gets starting index
+
+	if (strstr(source)) = NULL { //If the string is empty return -1
+		return(-1);
+
+	while (i < length) {
+		target[i] = source[from+i,n-1];//idk lol
+		return(0);
+		i++; //increment i
+}
+	printf("The substring is \"$s\"\n", target);
+}
 
 
 //main function
 int main() {
-	printf("%s\n", *safestrcat); //1 error: it is saying there is an error with %s, cant figure it out
+	printf("%d\n", *safestrcat); //1 error: it is saying there is an error with %s, cant figure it out
 }
 	
