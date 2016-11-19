4d6f609e665a738b5d4d10eaa8ff446192f67c3d
Brookealynn <brookeresendes@outlook.com>
2016-11-19 15:33:43 -0600

    Less errors but still needs help

--- Question2.c
+++ Question2.c
@@ -4,15 +4,14 @@
 #include<stdio.h>
 
 //Returns a new string containing the string in a followed by the string in b
-//does it automatically return?
-char *safestrcat(*a,*b) {
+char *safestrcat(char *a, char *b) {
 	char *x[255];
-	x = *strcat(a,b);
+	x = strcat(a,b);
 	return x; //return the new string x 
 }
 
 //main function
 int main() {
-	printf("%s\n", *safestrcat(char *a,char *b));
+	printf("%s\n", *safestrcat (char *a, char *b));
 }
 	
