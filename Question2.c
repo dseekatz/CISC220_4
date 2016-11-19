2cee81f4ac2a85f7782bed5dfa6ca6bec63e6b06
Brookealynn <brookeresendes@outlook.com>
2016-11-19 15:28:23 -0600

    Less errors but still needs to be fixed

--- Question2.c
+++ Question2.c
@@ -3,15 +3,16 @@
 #include<string.h>
 #include<stdio.h>
 
-//main function
-int main() {
-	printf("%s\n", *safestrcat());
-}
-
 //Returns a new string containing the string in a followed by the string in b
 //does it automatically return?
-char* safestrcat(char *a, char *b) {
-	char buf[255];
-	char str *x = strcat(a,b);
+char *safestrcat(*a,*b) {
+	char *x[255];
+	x = *strcat(a,b);
 	return x; //return the new string x 
-}	
+}
+
+//main function
+int main() {
+	printf("%s\n", *safestrcat(char *a,char *b));
+}
+	
