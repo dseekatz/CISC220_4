860cd62774e02d60715ef9cd9684b18b72f4824f
Brookealynn <brookeresendes@outlook.com>
2016-11-19 17:26:49 -0600

    Cant figure out why there is an error with %s

--- Question2.c
+++ Question2.c
@@ -5,21 +5,21 @@
 
 //Returns a new string containing the string in a followed by the string in b
 char *safestrcat(char *a, char *b) {
-	char *x[255]; //is it keyboard input for a and b?
+	char *x; //is it keyboard input for a and b?
 	x = strcat(a,b);
 	return x; //return the new string x 
 }
 
-int substring(char *source, int from, int n, char *target) {
-	char target[255];
-	if (strstr(source,from)) {
+//int substring(char *source, int from, int n, char *target) {
+//	char target;
+//	if (strstr(source,from)) {
 		
-	}
-}
+//	}
+//}
 
 
 //main function
 int main() {
-	printf("%s\n", *safestrcat (char *a, char *b));
+	printf("%s\n", *safestrcat); //1 error: it is saying there is an error with %s, cant figure it out
 }
 	
