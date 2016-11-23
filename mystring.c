77531d3756103c0b5c42052aad1905d5593398b3
Brooke <13brr2@queensu.ca>
2016-11-23 18:12:26 -0500

    Update mystring.c

--- mystring.c
+++ mystring.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "mystring.h"
 
+//safestrcat returns a new string containing string a followed by string b
 char* safestrcat(char *a, char *b) {
 	char *result = malloc(strlen(a)+strlen(b)+1);
 	strcat(result,a);
@@ -10,13 +11,14 @@ char* safestrcat(char *a, char *b) {
 	return result;
 }
 
+//substring gets a substring from within a string
 int substring(char *source, int from, int n, char *target) {
 	if (source == NULL || (n+from) > strlen(source)) {
 		fprintf(stderr,"Error!\n");
-		return -1;
+		return -1; //Return -1 upon failure
 	}
 	source += from;
 	memmove(target, source,n);
 	printf("%s\n", target);
-	return 0;
+	return 0; //Return 0 upon success
 }
