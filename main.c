bfc972a6ef9a2c541181dacbb61388a6202a3cac
Brookealynn <brookeresendes@outlook.com>
2016-11-23 16:09:34 -0600

    Completed main

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "mystring.h"
+
+//Main function to initate the entirety of Question 2 for this assignment
+int main() {
+	char* string1;
+	char target[255] = {0};
+	string1 = safestrcat("Apple","pie");
+	printf("%s\n", string1);
+	
+	substring("CISC220", 4,3, target);
+	return 0;
+}
