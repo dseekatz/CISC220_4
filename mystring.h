18c6833753b0f51fb09acca63704ab39acc42934
dseekatz <dseekatz@gmail.com>
2016-11-24 12:31:21 -0500

    Update mystring.h

--- mystring.h
+++ mystring.h
@@ -1,6 +1,12 @@
 #ifdef MYSTRING_H
 #define MYSTRING_H
 
+/* David Seekatz				16das4 20037710
+ *Sean Eylott				13se18 10141657
+ *Simon Ungar        			14seau 10149546
+ *Brooke Resendes   			13brr2 10141112
+ */
+
 //Contains the header of the safestrcat function
 char* safestrcat(char *a, char *b);
 int substring(char *source, int from, int n, char *target);
